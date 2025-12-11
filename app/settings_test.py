b7112c3d9ddafba267451037acc66417914fcdc3
issah <isabeli.romano@gmail.com>
2025-12-10 23:16:29 -0300

    chore: update Django settings for testing environment and remove redundant configuration in conftest.py

---
+++ app/settings_test.py
@@ -0,0 +1,10 @@
+"""Settings para testes - usa SQLite em memória."""
+from app.settings import *  # noqa: F401, F403
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    }
+}
+
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,19 +1,3 @@
 import os
 
-import django
-from django.conf import settings
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
-
-# API Key vazia = middleware permite acesso sem validação
 os.environ['API_KEY'] = ''
-
-
-def pytest_configure():
-    settings.DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': ':memory:',
-        }
-    }
-    django.setup()
